4d1d88d58e9a623634c22db36f2b58143d7c5a18
thatwys <956807158@qq.com>
2019-10-21 10:17:25 +0800

    linklist

--- DataStructure/linklist.c
+++ DataStructure/linklist.c
@@ -2,23 +2,66 @@ typedef struct Node
 {
     int data;
     struct Node *next;
-}Node,*Linklist;                 //链表数据结构
+} Node, *Linklist; //链表数据结构
 
+//链表生成（头部插入）
+void CreateList(Linklist *L, int n)
+{
+    Linklist p;
+    int i;
+
+    srand(time(0));
+
+    *L = (Linklist)malloc(sizeof(Node));
+    (*L)->next = NULL;
+
+    for (i = 0; i < n; i++)
+    {
+        p = (Linklist)malloc(sizeof(Node));
+        p->data = rand() % 100 + 1;
+        p->next = (*L)->next;
+        (*L)->next = p;
+    }
+}
+
+//链表生成（尾部插入）
+void CreateList(Linklist *L, int n)
+{
+    Linklist p, r;
+    int i;
+
+    srand(time(0));
+    *L = (Linklist)malloc(sizeof(Node));
+    r = *L;
+
+    for (i = 0; i < n; i++)
+    {
+        p = (Node *)malloc(sizeof(Node));
+        p->data = rand() % 100 + 1;
+        r->next = p;
+        r = p;
+    }
+
+    r->next = NULL;
+}
 
 //链表插入
-int ListInsert(Linklist *L,int i, int e){
+int ListInsert(Linklist *L, int i, int e)
+{
     int j;
     Linklist p, a;
 
     p = *L;
     j = 1;
 
-    while(p&& j<i){
-        p=p->next;
+    while (p && j < i)
+    {
+        p = p->next;
         j++;
     }
 
-    if(!p||j>i){
+    if (!p || j > i)
+    {
         return -1;
     }
 
@@ -30,21 +73,23 @@ int ListInsert(Linklist *L,int i, int e){
     return 0;
 }
 
- 
 //链表删除
-int ListDelete(Linklist *L, int i, int *e){
+int ListDelete(Linklist *L, int i, int *e)
+{
     int j;
-    Linklist p,q;
+    Linklist p, q;
 
     p = *L;
     j = 1;
 
-    while(p->next&&j<i){
+    while (p->next && j < i)
+    {
         p = p->next;
         j++;
     }
 
-    if( !(p->next)||j>i){
+    if (!(p->next) || j > i)
+    {
         return -1;
     }
 
@@ -53,5 +98,24 @@ int ListDelete(Linklist *L, int i, int *e){
 
     *e = q->data;
     free(q);
+    return 0;
+}
+
+//整表删除
+int ClearList(Linklist *L)
+{
+    Linklist p, q;
+
+    p = (*L)->next;
+
+    while (p)
+    {
+        q = p->next;
+        free(p);
+        p = q;
+    }
+
+    (*L)->next = NULL;
+
     return 0;
 }
